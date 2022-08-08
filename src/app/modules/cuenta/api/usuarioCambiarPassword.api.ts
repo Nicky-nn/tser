40e2b2057d955c301b26776422a2dc0fbe4340d9
Richard Quenta <rquenta@abot.one>
2022-08-08 13:16:41 -0400

    update password reset

---
+++ src/app/modules/cuenta/api/usuarioCambiarPassword.api.ts
@@ -0,0 +1,21 @@
+// noinspection GraphQLUnresolvedReference
+
+import {gql, GraphQLClient} from "graphql-request";
+import {AccessToken} from "../../../base/models/paramsModel";
+import {UsuarioCambiarPasswordInputProps} from "../interfaces/cuenta.interface";
+
+const gqlQuery = gql`
+    mutation CAMBIAR_PASSWORD($password: String!, $nuevoPassword: String!) {
+        usuarioCambiarPassword(password: $password, nuevoPassword: $nuevoPassword)
+    }
+`
+
+export const apiUsuarioCambiarPassword = async (input: UsuarioCambiarPasswordInputProps): Promise<Boolean> => {
+    const client = new GraphQLClient(import.meta.env.ISI_API_URL)
+    const token = localStorage.getItem(AccessToken)
+    // Set a single header
+    client.setHeader('authorization', `Bearer ${token}`)
+
+    const data: any = await client.request(gqlQuery, {password: input.password, nuevoPassword: input.nuevoPassword})
+    return data.usuarioCambiarPassword;
+}
