5d749e4bd11c11382a9b3984d1a7e29449d58070
Richard Quenta <richi617@gmail.com>
2022-12-20 12:24:40 -0400

    parametricas gift-cards

--- src/app/modules/giftCard/interfaces/inventario.interface.ts
+++
@@ -1,6 +0,0 @@
-import { SucursalProps } from '../../sucursal/interfaces/sucursal';
-
-export interface InventarioProps {
-  sucursal: SucursalProps;
-  stock: number;
-}
--- src/app/modules/giftCard/utils/clasificadores.ts
+++
@@ -1,7 +0,0 @@
-export const OPCIONES_PRODUCTO: Array<{ value: string; label: string }> = [
-  { value: 'Talla', label: 'Talla' },
-  { value: 'Tamanio', label: 'Tamanio' },
-  { value: 'Color', label: 'Color' },
-  { value: 'Material', label: 'Material' },
-  { value: 'Estilo', label: 'Estilo' },
-];
