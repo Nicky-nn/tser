51b846d78621faa4c86842a7f2796c9cd3b38560
Richard Quenta <richi617@gmail.com>
2024-03-18 23:18:57 -0400

    register: add react date picker css

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sf27tiqm8q8"
+    "revision": "0.4k6vbe7smv8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
